5a61dfeaf8ac76785fac6d05ad8e7005254853af
Abi Henkel <abij_ie@hotmail.com>
2023-09-05 12:49:27 +0100

    Unselecting a checkbox scenario added

--- cypress/e2e/Check_boxes.feature
+++ cypress/e2e/Check_boxes.feature
@@ -6,6 +6,10 @@ Background: Pre conditions
 Given I navigate to the webdriveruniversity homepage
 When I click the checkboxes button
 
-Scenario:
+Scenario: Selecting a check box
 And I select a check box
-Then the check box is selected
+Then the check box is marked checked
+
+Scenario: Unselecting a check box
+And I click a checkbox that is already checked
+Then the check box is no longer checked
--- cypress/support/step_definitions/Check_Box_Steps.js
+++ cypress/support/step_definitions/Check_Box_Steps.js
@@ -1,11 +1,17 @@
 import { Then, When } from "@badeball/cypress-cucumber-preprocessor";
 
-
-
 When(`I select a check box`, () => {
-    cy.get('#checkboxes > :nth-child(1) > input').click();
+  cy.get("#checkboxes > :nth-child(1) > input").click();
 });
 
-Then(`the check box is selected`, () => {
-
+Then(`the check box is marked checked`, () => {
+  cy.get("#checkboxes > :nth-child(1) > input").should("be.checked");
 });
+
+When(`I click a checkbox that is already checked`, () => {
+  cy.get("#checkboxes > :nth-child(5) > input").click();
+});
+
+Then(`the check box is no longer checked`, () => {
+    cy.get("#checkboxes > :nth-child(5) > input").should("not.be.checked"); 
+})
